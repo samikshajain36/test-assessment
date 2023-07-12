5f9dd98c79861d66f2ce8f010526a4708182e4b9
samikshajain36 <samikshajain3609@gmail.com>
2023-07-12 19:07:00 +0530

    HTTPS fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import axios from 'axios'
 const MovieList = () => {
   const [movie, setMovie] = useState([])
   const fetchData = async () => {
-  const response = await axios.get("http://jsonplaceholder.typicode.com/posts", {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/posts", {
     headers: { 
       'Access-Control-Allow-Origin' : '*',
       'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
